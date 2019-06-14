342cb50a2fb76988dadebaabd718d1d63bd0aa6e
Nicolas Bruy?re <nicolas.bruyere@nbb.be>
2019-06-14 13:56:07 +0200

    before summary

--- src/app/events/event-thumbnail-component.ts
+++ src/app/events/event-thumbnail-component.ts
@@ -1,30 +1,85 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
-
+/*
+ * ngSwitchCase -> structural directive
+ * [ngSwitch]
+ * [class.green] -> class binding
+ * [ngClass] -> expect object with boolean return
+ * */
 @Component({
   selector: "event-thumbnail",
   template: `
     <div class="well hoverwell thumbnail">
       <h2>{{ event.name }}</h2>
-      <p>
-        {{ event.date }}<br />
-        {{ event.time }}<br />
-        \${{ event.price }}<br />
-        {{ event.location.address }}
-        {{ event.location.city }}
-        {{ event.location.country }}
+      <div>Date: {{ event.date }}</div>
+      <div
+        [ngStyle]="getInlineStyle()"
+        [ngClass]="getAccentuation()"
+        [ngSwitch]="event?.time"
+      >
+        Time: {{ event?.time }}
 
-        <img src="{{ event.imageUrl }}" /><br />
-      </p>
+        <span *ngSwitchCase="'8:00 am'">Early time</span>
+        <span *ngSwitchCase="'10:00 am'">Late time</span>
+        <span *ngSwitchDefault>Normal time</span>
+      </div>
+      <div>Price: \${{ event.price }}</div>
+      <div *ngIf="event?.location">
+        Location:
+        <span>{{ event?.location?.address }}</span>
+        <span>{{ event?.location?.city }}</span>
+        <span>{{ event?.location?.country }}</span>
+        <div [hidden]="!event?.onlineUrl">
+          Online url: {{ event.onlineUrl }}<br />
+        </div>
+      </div>
     </div>
   `,
-  styles: []
+  styles: [
+    `
+      .thumbnail {
+        min-height: 250px;
+      }
+      .pad-left {
+        margin-left: 10px;
+      }
+      .well{
+        color: #bbb;
+      }
+      .green {
+        color: #51a351 !important;
+      }
+      .bold {
+        font-weight: bold;
+      }
+    `
+  ]
 })
 export class EventThumbnailComponent {
   /*
-   * @Input () : decorator to passe value
+   * @Input () : decorator to passe value"
    * @Output () : to communicate with a parent
    * EventEmitter : is an angular thing
    * */
   @Input() event: any;
-  @Output() eventClick = new EventEmitter();
+  @Output() testClick = new EventEmitter();
+  // @Output() someValue: string = "test Value";
+
+  private getHighlight() {
+    const earlyStart = this.event && this.event.time === "8:00 am";
+
+    return {
+      green: earlyStart,
+      bold: earlyStart
+    };
+  }
+  private getAccentuation(): string {
+    if (this.event && this.event.time === "8:00 am") {
+      return "green bold";
+    }
+  }
+  private getInlineStyle(): object {
+    if (this.event && this.event.time === "8:00 am") {
+      return { "font-style": "italic" };
+    }
+  }
 }
