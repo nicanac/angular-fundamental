88c4db151fac723a47efaf82d0f7cfbbf99c7da7
Nicolas Bruy?re <nicolas.bruyere@nbb.be>
2019-06-17 12:03:58 +0200

    wrapping a 3rd party libraries

---
+++ src/app/common/toastr.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+
+/*
+ * We must declare toastr as a variable, otherwise the IDE think it's an undeclared variable
+ * */
+declare let toastr: any;
+
+@Injectable({
+  providedIn: "root"
+})
+export class ToastrService {
+  public success(message: string, title?: string) {
+    toastr.success(message, title);
+  }
+  public warning(message: string, title?: string) {
+    toastr.success(message, title);
+  }
+  public info(message: string, title?: string) {
+    toastr.success(message, title);
+  }
+  public error(message: string, title?: string) {
+    toastr.success(message, title);
+  }
+}
