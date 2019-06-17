4cb4fe75947c9858fc9d282e7905d61c6e252e78
Nicolas Bruy?re <nicolas.bruyere@nbb.be>
2019-06-17 12:15:44 +0200

    add eventlist const

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,1 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-
-import { EventsAppComponent } from "./events-app.component";
-import { EventsListComponent } from "./events/events-list.component";
-import {EventThumbnailComponent} from "./events/event-thumbnail-component";
-import { NavBarComponent } from './navbar/navbar.component';
-import {EventService} from './events/shared/event.service';
-
-@NgModule({
-  imports: [BrowserModule],
-  declarations: [EventsAppComponent, EventsListComponent, EventThumbnailComponent, NavBarComponent],
-  // Angular injector is aware of this and if we need we can import it in another document and will know where to go
-  // to get this service
-  // after you need to inject it in your component (in a the EnventList class constructor for instance)
-  providers: [EventService],
-  bootstrap: [EventsAppComponent]
-})
-export class AppModule {}
+import { BrowserModule } from "@angular/platform-browser";import { NgModule } from "@angular/core";import { EventsAppComponent } from "./events-app.component";import { EventsListComponent } from "./events/events-list.component";import { EventThumbnailComponent } from "./events/event-thumbnail-component";import { NavBarComponent } from "./navbar/navbar.component";import { EventService } from "./events/shared/event.service";import { ToastrService } from "./common/toastr.service";@NgModule({  imports: [BrowserModule],  declarations: [    EventsAppComponent,    EventsListComponent,    EventThumbnailComponent,    NavBarComponent  ],  // Angular injector is aware of this and if we need we can import it in another document and will know where to go  // to get this service  // after you need to inject it in your component (in a the EnventList class constructor for instance)  providers: [EventService, ToastrService],  bootstrap: [EventsAppComponent]})export class AppModule {}
